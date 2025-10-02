d1000c6ea32fdf6d0ae075395df9d496ae9ccf44
Peketruli <raulgonzalez20@arrigorriagabhi.net>
2025-10-02 17:59:09 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,3 @@
-// Equipos y pilotos
 const EQUIPOS = [
   { id: 1, nombre: "Scuderia Ferrari", pilotos: ["Carlos Sainz", "Charles Leclerc"] },
   { id: 2, nombre: "Mercedes AMG", pilotos: ["Lewis Hamilton", "George Russell"] },
@@ -10,18 +9,4 @@ const EQUIPOS = [
   { id: 8, nombre: "AlphaTauri", pilotos: ["Yuki Tsunoda", "Daniel Ricciardo"] },
   { id: 9, nombre: "Haas", pilotos: ["Kevin Magnussen", "Nico Hülkenberg"] },
   { id: 10, nombre: "Sauber", pilotos: ["Valtteri Bottas", "Zhou Guanyu"] },
-];
-
-// Carreras
-const CARRERAS = [
-  { id: 1, nombre: "GP de Australia", circuito: "Melbourne" },
-  { id: 2, nombre: "GP de Mónaco", circuito: "Mónaco" },
-  { id: 3, nombre: "GP de Gran Bretaña", circuito: "Silverstone" },
-  { id: 4, nombre: "GP de Italia", circuito: "Monza" },
-  { id: 5, nombre: "GP de Bélgica", circuito: "Spa" },
-  { id: 6, nombre: "GP de Singapur", circuito: "Singapur" },
-  { id: 7, nombre: "GP de Japón", circuito: "Suzuka" },
-  { id: 8, nombre: "GP de Canadá", circuito: "Montreal" },
-  { id: 9, nombre: "GP de Brasil", circuito: "Interlagos" },
-  { id: 10, nombre: "GP de Abu Dhabi", circuito: "Yas Marina" },
 ];
